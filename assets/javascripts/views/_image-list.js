0a3480e94044ac8c00ede82deabdaffa86c98ee1
Daniel James Williams <mail@danielwilliams.dk>
2014-10-03 20:38:19 +0200

    Cleanup

--- assets/javascripts/views/_image-list.js
+++ assets/javascripts/views/_image-list.js
@@ -2,20 +2,17 @@ var App = App || {};
 App.Views = App.Views || {};
 
 App.Views.ImageList = (function () {
-  // This is a private constructor.
+
   var Struct = Backbone.View.extend({
     el: $('body'),
     initialize: function () {}
   });
 
-  // This is an instance of the constructor above
   var newObject = new Struct();
 
   return {
     STRUCT: Struct,
     OBJECT: newObject
   };
-})();
 
-// QUESTION 1) Is a Constructor and Struct the same?
-// QUESTION 2) What is the rule for return uppercase naming?
+})();
