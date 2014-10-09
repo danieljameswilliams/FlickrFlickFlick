c624d7b2d85f9245b33b8b48633be6d60e510ce9
Daniel James Williams <mail@danielwilliams.dk>
2014-10-09 13:42:49 +0200

    Now working on a localhost server

--- app.js
+++ app.js
@@ -3,21 +3,20 @@ var handlebars = require('express-handlebars');
 var path = require('path');
 
 var app = express();
-var viewsFolder = __dirname + '/public/views';
+var viewsFolder = __dirname + '/public/templates';
 
 app.use(express.static(__dirname + '/public'));
 
 app.engine( 'handlebars', handlebars(
 {
-  defaultLayout: viewsFolder + '/layouts/master',
-  layoutsDir: viewsFolder + '/layouts',
-  partialsDir: viewsFolder + '/partials'
+  defaultLayout: viewsFolder + '/master',
+  layoutsDir: viewsFolder + '/layouts'
 }) );
 app.set('view engine', 'handlebars');
 app.set('views', viewsFolder);
 
 app.get('/', function (req, res) {
-  res.render('home');
+  res.render('layouts/image-list');
 });
 
 app.listen(3000);
